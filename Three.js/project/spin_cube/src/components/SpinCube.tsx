0952e2d20b77f3072305e7982182c9218159050f
kub938 <kub938@naver.com>
2025-08-26 16:33:41 +0900

    feat: 기본적인 사각형 렌더링 구현

--- Three.js/project/spin_cube/src/components/SpinCube.tsx
+++ Three.js/project/spin_cube/src/components/SpinCube.tsx
@@ -10,17 +10,33 @@ export default function ThreeScene() {
     if (!canvasRef.current) return;
 
     const scene = new THREE.Scene();
+    if (scene) {
+      console.log("scene 생성 완료");
+      console.log(scene);
+    }
+    const camera = new THREE.PerspectiveCamera(100, 800 / 600, 0.1, 1000);
 
-    const camera = new THREE.PerspectiveCamera(75, 800 / 600, 0.1, 1000);
-    camera.position.z = 5;
+    camera.position.set(3, 3, 5);
+
+    if (camera) {
+      console.log("camera 생성 완료");
+      console.log(camera);
+    }
 
     const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
     renderer.setSize(800, 600);
+
+    if (renderer) {
+      console.log("renderer 생성 완료");
+      console.log(renderer);
+    }
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
+
+    renderer.render(scene, camera);
   });
 
   return (
