c75bcfcb2ad92e29c1a69214b34a27a32d671d30
kub938 <kub938@naver.com>
2025-08-26 18:37:53 +0900

    feat: 명암주기

--- Three.js/project/spin_cube/src/components/SpinCube.tsx
+++ Three.js/project/spin_cube/src/components/SpinCube.tsx
@@ -10,33 +10,52 @@ export default function ThreeScene() {
     if (!canvasRef.current) return;
 
     const scene = new THREE.Scene();
-    if (scene) {
-      console.log("scene 생성 완료");
-      console.log(scene);
-    }
-    const camera = new THREE.PerspectiveCamera(100, 800 / 600, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(100, 1, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
 
     camera.position.set(3, 3, 5);
-
-    if (camera) {
-      console.log("camera 생성 완료");
-      console.log(camera);
-    }
-
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
     renderer.setSize(800, 600);
 
-    if (renderer) {
-      console.log("renderer 생성 완료");
-      console.log(renderer);
-    }
-
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x00ff00,
+      shininess: 100,
+    });
+
     const cube = new THREE.Mesh(geometry, material);
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(5, 5, 5);
+    scene.add(light);
     scene.add(cube);
 
-    renderer.render(scene, camera);
+    let time = 0;
+    const cube_spin_animate = () => {
+      // cube.rotation.y += (Math.PI * 2) / 60; //초당 1바퀴
+      // cube.rotation.y += (Math.PI * 2) / 120; //2초당 1바퀴
+
+      time += 0.01;
+      // const speedX = Math.sin(time) * 0.02;
+      const speedY = Math.sin(time * 1.5) * 0.015;
+      // const speedZ = Math.sin(time * 0.8) * 0.01;
+      cube.rotation.y += speedY;
+      // cube.rotation.x += speedX;
+      // cube.rotation.z += speedZ;
+
+      const red = (Math.sin(time * 0.5) + 1) / 2;
+      const green = (Math.sin(time * 0.2) + 1) / 2;
+      const blue = (Math.sin(time * 0.3) + 1) / 2;
+      material.color.setRGB(red, green, blue);
+
+      // cube.position.y = Math.sin(time * 2) * 0.3; // 위아래로
+      // cube.position.x = Math.cos(time * 1.5) * 0.2;
+      // camera.position.x = Math.cos(time) * 5;
+      // camera.position.y = Math.sin(time) * 5;
+      // camera.lookAt(0, 0, 0);
+      renderer.render(scene, camera);
+      requestAnimationFrame(cube_spin_animate);
+    };
+
+    cube_spin_animate();
   });
 
   return (
