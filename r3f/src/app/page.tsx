ad5e3898df855e9f5548ddee99c0fd8460e55165
kub938 <kub938@naver.com>
2025-10-02 21:26:40 +0900

    learn: 글씨로된 구체 구현 공부

--- r3f/src/app/page.tsx
+++ r3f/src/app/page.tsx
@@ -1,25 +1,34 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import styles from "./page.module.css";
-import { OrbitControls } from "@react-three/drei";
-import Box from "@/components/Cube";
+import { TrackballControls } from "@react-three/drei";
+import { Suspense } from "react";
+import Cloud from "@/components/WordBall";
 
 export default function App() {
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <OrbitControls />
+    <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 35], fov: 90 }}>
+      <fog attach="fog" args={["#202025", 0, 80]} />
+      <Suspense fallback={null}>
+        <group rotation={[10, 10.5, 10]}>
+          <Cloud count={8} radius={20} />
+        </group>
+      </Suspense>
+      <TrackballControls />
     </Canvas>
+    // <Canvas>
+    //   <ambientLight intensity={Math.PI / 2} />
+    //   <spotLight
+    //     position={[10, 10, 10]}
+    //     angle={0.15}
+    //     penumbra={1}
+    //     decay={0}
+    //     intensity={Math.PI}
+    //   />
+    //   <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+    //   <Box position={[-1.2, 0, 0]} />
+    //   <Box position={[1.2, 0, 0]} />
+    //   <OrbitControls />
+    // </Canvas>
   );
 }
