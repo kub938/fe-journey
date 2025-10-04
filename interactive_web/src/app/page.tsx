d30cd8f9d12f97a7c32a61d98fd8fa2097327526
kub938 <kub938@naver.com>
2025-10-05 00:23:47 +0900

    chore: Card props rotate 추가

--- interactive_web/src/app/page.tsx
+++ interactive_web/src/app/page.tsx
@@ -1,16 +1,17 @@
 import Card from "@/components/Card";
 import styles from "./page.module.css";
 import FadeIn from "./(scroll)/fadein/FadeIn";
+import Perspective from "@/components/Perspective";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Card rotateY={15} />
         <Card />
         <Card />
         <Card />
-        <Card />
-        <Card />
+        <Card rotateY={15} />
         <FadeIn direction="left" duration={0.5} delay={0}>
           <Card />
         </FadeIn>
