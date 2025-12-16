9177d795635e39e478a7eff6fe47a5dac0d7979a
kub938 <kub938@naver.com>
2025-12-16 23:25:54 +0900

    solved: 백준_2630_색종이_만들기 풀이 완료

---
+++ Algorithm/solved/백준_2630_색종이_만들기/백준_2630_색종이_만들기.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const file_path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(file_path).toString().trim().split("\n");
+const n = input[0];
+const board = input.slice(1).map((e) => e.split(" ").map(Number));
+
+let whiteCnt = 0;
+let blueCnt = 0;
+
+function check(half, r, c) {
+  for (let i = r; i < r + half; i++) {
+    for (let j = c + 1; j < c + half; j++) {
+      if (board[i][j] !== board[i][j - 1]) {
+        return false;
+      }
+    }
+  }
+
+  board[r][c] === 0 ? whiteCnt++ : blueCnt++;
+  return true;
+}
+
+function dfs(n, r, c) {
+  if (check(n, r, c)) {
+    return;
+  }
+
+  const half = n / 2;
+  dfs(half, r, c);
+  dfs(half, r, c + half);
+  dfs(half, r + half, c);
+  dfs(half, r + half, c + half);
+}
+
+dfs(n, 0, 0);
+
+console.log(blueCnt, whiteCnt);
