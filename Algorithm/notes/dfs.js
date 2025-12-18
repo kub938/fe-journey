78ffb49069e8d720e2c8faa8c1f02b7bb547e40e
kub938 <kub938@naver.com>
2025-12-18 23:17:02 +0900

    learn: dfs 재학습

--- Algorithm/notes/dfs.js
+++ Algorithm/notes/dfs.js
@@ -1,17 +1,40 @@
+// const graph = {
+//   1: [2, 3],
+//   2: [4],
+//   3: [4, 5],
+//   4: [],
+//   5: [],
+// };
+
+// const dfs = (here, visited = new Set()) => {
+//   if (visited.has(here)) return;
+
+//   visited.add(here);
+//   console.log(here);
+//   graph[here].forEach((e) => dfs(e, visited));
+// };
+
+// dfs(1);
+
 const graph = {
-  1: [2, 3],
-  2: [4],
-  3: [4, 5],
+  0: [1, 2],
+  1: [4, 3],
+  2: [5, 6],
+  3: [],
   4: [],
   5: [],
+  6: [],
 };
+function dfsExample(depth) {
+  // if (depth === graph.length) {
+  //   console.log("최대 depth 도착");
+  //   return;
+  // }
 
-const dfs = (here, visited = new Set()) => {
-  if (visited.has(here)) return;
+  graph[depth].forEach((g) => {
+    console.log(g, "노드 들어가요");
+    dfsExample(g);
+  });
+}
 
-  visited.add(here);
-  console.log(here);
-  graph[here].forEach((e) => dfs(e, visited));
-};
-
-dfs(1);
+dfsExample(0);
